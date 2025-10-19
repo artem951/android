1abb11399268a09131720e0abad2f27cc867c49a
artem951 <artemerofeev2006@gmail.com>
2025-10-20 03:38:01 +0700

    bob

--- src/Human.kt
+++ src/Human.kt
@@ -1,3 +1,22 @@
+fun main() {
+    val humans = arrayOf(
+        Human("Иван Иванов", 20, 1.0),
+        Human("Пётр Петров", 22, 1.2),
+        Human("Сергей Сидоров", 19, 0.8)
+    )
+
+    val simulationTime = 10
+
+    for (t in 1..simulationTime) {
+        println("---- Шаг $t ----")
+        humans.forEach {
+            it.move()
+            println(it)
+        }
+        Thread.sleep(500)
+    }
+}
+
 class Human(
     var fullName: String,
     var age: Int,
@@ -7,13 +26,12 @@ class Human(
     var y: Double = 0.0
 
     fun move() {
-        val dx = (-1..1).random().toDouble()
-        val dy = (-1..1).random().toDouble()
-        x += dx * currentSpeed
-        y += dy * currentSpeed
+        val angle = Math.random() * 2 * Math.PI
+        x += currentSpeed * kotlin.math.cos(angle)
+        y += currentSpeed * kotlin.math.sin(angle)
     }
 
     override fun toString(): String {
-        return "$fullName (возраст $age): позиция (x=${"%.2f".format(x)}, y=${"%.2f".format(y)})"
+        return "$fullName (возраст $age): координаты (x=${"%.2f".format(x)}, y=${"%.2f".format(y)})"
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val humans = arrayOf(
-        Human("Иван Иванов", 20, 1.0),
-        Human("Пётр Петров", 22, 1.2),
-        Human("Сергей Сидоров", 19, 0.8)
-    )
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
 
-    val simulationTime = 10
-
-    for (t in 1..simulationTime) {
-        println("---- Шаг $t ----")
-        humans.forEach {
-            it.move()
-            println(it)
-        }
-        Thread.sleep(500)
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
     }
 }
-
